6f44d7798fab3702c5bc3b3fe246207c67cd2710
shetty manasa <manasa059shetty@gmail.com>
2026-07-08 19:33:31 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,6 @@ export default function App() {
   const [user, setUser] = useState(null)
   const [role, setRole] = useState(null)
   const [loading, setLoading] = useState(true)
-  const snap = await getDoc(doc(db, 'users', u.uid))
-if (snap.exists()) setRole(snap.data().role)
   
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, async (u) => {
